0394dc3e07a175e67d97afec9f796369dcf45261
sinokadev <sinoka@sinoka.dev>
2026-08-23 14:57:19 +0900

    add target and script

--- script/demo_release_build.sh
+++ script/demo_release_build.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
 cmake -S . -B build -DCMAKE_BUILD_TYPE=Release
+echo demo build
 cmake --build build --target demo
+echo benchmark build
+cmake --build build --target benchmark
+echo scene_render build
+cmake --build build --target scene_render
